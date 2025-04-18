1b5d0246f87645167b4a5b9618e14a21c9eaf41b
clément chassagne <clement.c.chassagne@airbus.com>
2025-04-18 16:58:57 +0200

    eow

--- package/structarray/parself2.py
+++ package/structarray/parself2.py
@@ -92,12 +92,11 @@ class ElfParser() :
 		
 		self.r_map = collections.defaultdict(dict)
 		self.s_map = dict() # liste des symboles de haut niveau
+		self.p_map = dict() # reverse type definitions, get parents 
 
 		self.typedef_map = dict()
 		self.variable_map = dict()
 		self.base_map = dict()
-
-		self.default_name = None
 
 		for top in self.load(elf_pth) :
 			self.parse(top)
@@ -105,27 +104,23 @@ class ElfParser() :
 
 		Path("r_map.json").save(self.r_map, verbose=True)
 		Path("s_map.json").save(self.s_map, verbose=True)
+		Path("p_map.json").save(self.p_map, verbose=True)
+		Path("typedef_map.json").save(self.typedef_map, verbose=True)
+		Path("variable_map.json").save(self.variable_map, verbose=True)
+		Path("base_map.json").save(self.base_map, verbose=True)
 
-		w_lst = list()
-		for i, m_lst in enumerate(self.walk('_C_MfcAfcs')) :
-			w_lst.append(self.expand(m_lst))
-			if i > 100 :
-				break
-		Path("walk.txt").write_text('\n'.join(w_lst))
+		# w_lst = list()
+		# for i, m_lst in enumerate(self.walk('_C_MfcAfcs')) :
+		# 	w_lst.append(self.expand(m_lst))
+		# 	if i > 100 :
+		# 		break
+		# Path("walk.txt").write_text('\n'.join(w_lst))
 
-		w_lst = list()
-		for i, m_lst in enumerate(self.walk('_C_MfcAfcs')) :
-			w_lst.append(' > '.join(str(m) for m in m_lst))
-			if i > 100 :
-				break
-		Path("walk.brut").write_text('\n'.join(w_lst))
 
-		w_lst = list()
-		for i, m_lst in enumerate(self.walk('_C_MfcAfcs')) :
-			w_lst.append(self.expand_struct(m_lst))
-			if i > 100 :
-				break
-		Path("walk.tsv").save(w_lst)
+		# w_lst = list()
+		# for i, m_lst in enumerate(self.walk('_C_MfcAfcs')) :
+		# 	w_lst.append(self.expand_struct(m_lst))
+		# Path("walk.tsv").save(w_lst)
 
 	def run(self, name, mapping_pth, is_relative=True, is_compact=True) :
 
@@ -154,10 +149,8 @@ class ElfParser() :
 		return u
 
 	def resolve(self, name_or_oid) :
-
 		if isinstance(name_or_oid, str) :
 			name = name_or_oid
-			# name can either be the name of a global variable or a the name of a typedef
 			if name in self.variable_map :
 				oid = self.variable_map[name]
 			elif name in self.typedef_map :
@@ -167,16 +160,19 @@ class ElfParser() :
 		elif isinstance(name_or_oid, int) :
 			oid = name_or_oid
 
-		if oid in self.s_map :
-			# print(f"S_MAP {oid} -> {self.s_map[oid]}")
-			oid = self.s_map[oid]
-		while oid in self.r_map and isinstance(self.r_map[oid], (Typedef, Pointer)) :
-			# print(f"R_MAP {pident} -> {self.r_map[pident][0]}")
-			oid = self.r_map[oid].oid
+		# if oid in self.s_map :
+		# 	oid = self.s_map[oid]
+
+		# while oid in self.r_map and isinstance(self.r_map[oid], (Typedef, Pointer)) :
+		# 	oid = self.r_map[oid].oid
 
 		print(f">>> resolve({name_or_oid}) -> {oid}/{self.r_map[oid]}")
 		
 		return oid
+
+	def sizeof(self, name_or_oid) :
+		oid = self.resolve(name_or_oid)
+		sizeof = self.r_map[oid].size
 
 	def to_base(self, q) :
 		while isinstance(q, Typedef) :
@@ -216,87 +212,116 @@ class ElfParser() :
 		else :
 			return ".".join(s_lst), f"P{obj.sizeof}", offset
 
+	def iter_by_offset(self, name) :
+		# take a m_lst, return the path to the object
+		for m_lst in self.walk(name, follow_pointer=False) :
+			s_lst = list()
+			for oid, t_lst, obj, offset in m_lst :
+				if isinstance(obj, Member) :
+					s_lst.append(obj.name)
+
+			oid, t_lst, obj, offset = m_lst[-1]
+			if isinstance(obj, Base) :
+				yield s_lst, f"{obj.letter}{obj.sizeof}", offset
+			else :
+				yield s_lst, f"P{obj.sizeof}", offset
+
+	def get_root(self, name) :
+		if name in self.variable_map :
+			oid = self.variable_map[name]
+		elif name in self.typedef_map :
+			oid = self.typedef_map[name]
+		else :
+			if isinstance(name, int) :
+				oid = name
+			else :
+				raise ValueError(f"Can't find a reference to {name} either as a variable or as a type")
+
+		obj = self.r_map[oid]
+
+		assert isinstance(obj, Typedef)
+
+		return oid, list(), obj, 0
+
 	def walk(self, name, max_depth=None, follow_pointer=False) :
 
-		Path("walk.raw").write_text('')
-		oid = self.resolve(name)
-		m_lst = [(oid, self.r_map[oid], 0),]
+		# TODO: au lieu de mettre un simple oid, on peut mettre un o_lst qui cumule la liste des oid traversés... ou alors juste le un champ oid type
 
-		yield from self._walk(m_lst, max_depth, follow_pointer)
+		yield from self._walk([self.get_root(name),], max_depth, follow_pointer)
 
 	def _walk(self, m_lst, max_depth, follow_pointer, depth=0) :
 		
-		print("\t" + "-" * (depth+1) + "> " + f"{self.expand(m_lst)}", max_depth, depth)
-		with Path("walk.raw").open('at') as fid :
-		 	fid.write(str(m_lst) + '\n')
+		# print("\t" + "-" * (depth+1) + "> " + f"{self.expand(m_lst)}", max_depth, depth)
+		# with Path("walk.raw").open('at') as fid :
+		#  	fid.write(str(m_lst) + '\n')
 
-		oid, obj, offset = m_lst[-1]
+		oid, t_lst, obj, offset = m_lst[-1]
 
 		match obj :
 			case Base() :
 				yield m_lst
 			case Typedef() :
-				yield from self._walk(m_lst + [(obj.oid, self.r_map[obj.oid], offset),], max_depth, follow_pointer, depth)
+				yield from self._walk(m_lst + [(obj.oid, t_lst + [oid,], self.r_map[obj.oid], offset),], max_depth, follow_pointer, depth)
 			case Member() :
-				yield from self._walk(m_lst + [(obj.oid, self.r_map[obj.oid], offset),], max_depth, follow_pointer, depth+1)
+				yield from self._walk(m_lst + [(obj.oid, list(), self.r_map[obj.oid], offset),], max_depth, follow_pointer, depth+1)
 			case Structure() :
 				if max_depth is None or depth <= max_depth :
 					for i, sub in enumerate(self.r_map[oid].detail) :
-						yield from self._walk(m_lst + [(f"{i}", sub, offset + sub.offset),], max_depth, follow_pointer, depth)
+						yield from self._walk(m_lst + [(f"{i}", t_lst, sub, offset + sub.offset),], max_depth, follow_pointer, depth)
 				else :
 					yield m_lst
 			case Pointer() :
 				if follow_pointer :
-					yield from self._walk(m_lst + [(obj.oid, self.r_map[obj.oid], offset),], max_depth, follow_pointer, depth)
+					yield from self._walk(m_lst + [(obj.oid, list(), self.r_map[obj.oid], offset),], max_depth, follow_pointer, depth)
 				else :
 					yield m_lst
 
-	def walk_smart(self, name=None, max_depth=None, follow_pointer=False) :
-		pident = self.get_oid(self.default_name if name is None else name)
+	# def walk_smart(self, name=None, max_depth=None, follow_pointer=False) :
+	# 	pident = self.get_oid(self.default_name if name is None else name)
 
-		self.sizeof = self.r_map[pident].size
+	# 	self.sizeof = self.r_map[pident].size
 
-		yield from self._walk_smart(pident, list(), max_depth, follow_pointer)
+	# 	yield from self._walk_smart(pident, list(), max_depth, follow_pointer)
 
-	def _walk_smart(self, pident, m_lst, max_depth, follow_pointer, depth=0) : 
-		if m_lst :
-			pname, pcount, ptype, poffset = m_lst[-1]
-		else :
-			pname, pcount, ptype, poffset = None, None, None, 0
+	# def _walk_smart(self, pident, m_lst, max_depth, follow_pointer, depth=0) : 
+	# 	if m_lst :
+	# 		pname, pcount, ptype, poffset = m_lst[-1]
+	# 	else :
+	# 		pname, pcount, ptype, poffset = None, None, None, 0
 
-		q = self.r_map[pident]
+	# 	q = self.r_map[pident]
 
-		match q :
-			case Base() :
-				m_lst[-1] = (pname, 1, f"{q.mtype}{q.msize}", poffset)
-				yield m_lst
-			case Typedef() :
-				# print("TYPEDEF", pname, depth, max_depth, max_depth is None or depth <= max_depth)
-				m_lst[-1] = (pname, 1, q.alias, poffset)
-				yield from self._walk(q.type, m_lst, max_depth, depth+1)
-			case Structure() :
-				# print("STRUCT ", pname, depth, max_depth, max_depth is None or depth <= max_depth)
-				if max_depth is None or depth <= max_depth :
-					for m in self.r_map[pident].detail :
-						yield from self._walk(m.oid, m_lst + [(m.name, 1, None, poffset + m.offset),], max_depth, follow_pointer, depth+1)
-				else :
-					yield m_lst
-			case Pointer() :
-				# self.r_map[q.type].name
-				if follow_pointer :
-					while pident in self.r_map and isinstance(self.r_map[pident], (Typedef, Pointer)) :
-						# print(f"R_MAP {pident} -> {self.r_map[pident][0]}")
-						pident = self.r_map[pident].type
-					for m in self.r_map[pident].detail :
-						yield from self._walk(m.type, m_lst + [(m.name + '*', 1, None, poffset + m.offset),], max_depth, follow_pointer, depth+1)
-				else :
-					m_lst[-1] = (pname, 0, f"P{q.size}", poffset)
-					yield m_lst
-			case Array() :
-				m_lst[-1] = (pname, q.shape, self.to_base(self.r_map[q.type]).mtype, poffset)
-				yield m_lst
-			case _ :
-				raise ValueError(m_lst, q)
+	# 	match q :
+	# 		case Base() :
+	# 			m_lst[-1] = (pname, 1, f"{q.mtype}{q.msize}", poffset)
+	# 			yield m_lst
+	# 		case Typedef() :
+	# 			# print("TYPEDEF", pname, depth, max_depth, max_depth is None or depth <= max_depth)
+	# 			m_lst[-1] = (pname, 1, q.alias, poffset)
+	# 			yield from self._walk(q.type, m_lst, max_depth, depth+1)
+	# 		case Structure() :
+	# 			# print("STRUCT ", pname, depth, max_depth, max_depth is None or depth <= max_depth)
+	# 			if max_depth is None or depth <= max_depth :
+	# 				for m in self.r_map[pident].detail :
+	# 					yield from self._walk(m.oid, m_lst + [(m.name, 1, None, poffset + m.offset),], max_depth, follow_pointer, depth+1)
+	# 			else :
+	# 				yield m_lst
+	# 		case Pointer() :
+	# 			# self.r_map[q.type].name
+	# 			if follow_pointer :
+	# 				while pident in self.r_map and isinstance(self.r_map[pident], (Typedef, Pointer)) :
+	# 					# print(f"R_MAP {pident} -> {self.r_map[pident][0]}")
+	# 					pident = self.r_map[pident].type
+	# 				for m in self.r_map[pident].detail :
+	# 					yield from self._walk(m.type, m_lst + [(m.name + '*', 1, None, poffset + m.offset),], max_depth, follow_pointer, depth+1)
+	# 			else :
+	# 				m_lst[-1] = (pname, 0, f"P{q.size}", poffset)
+	# 				yield m_lst
+	# 		case Array() :
+	# 			m_lst[-1] = (pname, q.shape, self.to_base(self.r_map[q.type]).mtype, poffset)
+	# 			yield m_lst
+	# 		case _ :
+	# 			raise ValueError(m_lst, q)
 
 	def chrono(self, label) :
 		self._time_lst.append(time.time())
@@ -325,6 +350,10 @@ class ElfParser() :
 			func = f"_parse_{tag}"
 			try :
 				getattr(self, func)(child)
+				# try :
+				# 	self.p_map[self.r_map[child.offset].oid] = child.offset
+				# except :
+				# 	pass
 				if 'DW_AT_sibling' in child.attributes :
 					self.s_map[child.attributes['DW_AT_sibling'].value] = child.offset
 			except (AttributeError, KeyError) :
@@ -345,7 +374,7 @@ class ElfParser() :
 			die.attributes['DW_AT_name'].value.decode('utf8')
 		)
 		self.r_map[die.offset] = p
-		self.typedef_map[p.alias] = p.oid
+		self.typedef_map[p.alias] = die.offset
 
 	def _parse_array_type(self, die) :
 		u_lst = list()
@@ -383,6 +412,7 @@ class ElfParser() :
 				die.attributes['DW_AT_name'].value.decode('utf8')
 			)
 			self.r_map[die.offset] = p
+			
 	def _parse_structure_type(self, die) :
 		m_lst = list()
 		for i, child in enumerate(die.iter_children()) :
@@ -412,3 +442,11 @@ class ElfParser() :
 
 if __name__ == '__main__' :
 	u = ElfParser(Path(sys.argv[1]))
+	p = 'unitest_context'
+	p = '_C_MfcAfcs'
+	p = 704033
+
+	Path("walk_by_offset.tsv").save([m + [p, o]  for m, p, o in u.iter_by_offset(p)])
+
+	w_lst = [m_lst for m_lst in u.walk(p)][:100]
+	Path("walk_raw.tsv").save(w_lst)
